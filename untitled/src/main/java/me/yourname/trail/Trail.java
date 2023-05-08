0b02b3f447b9c308d4abf65b767cec1d472069c5
SalmonDesert506 <128499765+SalmonDesert506@users.noreply.github.com>
2023-05-08 11:37:10 +0200

    Add files via upload

---
+++ untitled/src/main/java/me/yourname/trail/Trail.java
@@ -0,0 +1,18 @@
+package me.yourname.trail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Trail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
